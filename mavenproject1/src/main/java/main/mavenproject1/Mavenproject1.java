1faf42d0fc368d03c61beca22f93ff550dcf4fd3
SAlA-9 <SAlA-9@SI9-DOC.salas.local>
2024-09-30 09:05:16 -0500

    Cargue de segundo proyecto, subir proyecto final, prueba de segundo commit

---
+++ mavenproject1/src/main/java/main/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.mavenproject1;
+
+/**
+ *
+ * @author SAlA-9
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba del primer commit de silvia");
+    }
+}
