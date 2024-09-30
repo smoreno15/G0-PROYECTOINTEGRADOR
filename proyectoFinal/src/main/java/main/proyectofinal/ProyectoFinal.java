dcf1aaa412fa27ed8ecd92b4133422923e407a99
SAlA-9 <SAlA-9@SI9-DOC.salas.local>
2024-09-30 08:30:39 -0500

    Validacion de nuevo proyecto, subir proyecto final, prueba de primer commit

---
+++ proyectoFinal/src/main/java/main/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.proyectofinal;
+
+/**
+ *
+ * @author SAlA-9
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
